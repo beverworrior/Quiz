90c2d305b20e5923b569237a0fd3e29ed4cd203e
Jan-Robert Nielsen <robertnielsen33@gmail.com>
2024-03-07 08:31:47 +0000

    added the question

--- README.md
+++ README.md
@@ -5,17 +5,15 @@ this quiz is for the person that loves to watch movies and can identify the movi
 1. ![step1](firstprogress1.png)
 2. ![step2](cssprogress.png)
 3. ![step3](Secoundcss.png)
-4. ![step4]()
-5. ![finalstep]()
-
-### Features
-1. 
+4. ![finalstep](Finalcssimages.png)
 
 ## Validator Testing
-1. ![HTML]()
-2. ![CSS]()
+1. ![HTML](htmlvalidate.png)
+2. ![CSS](cssvalidate.png)
 
-## Deployment
+## Improvement
+1. I can add a scoring system to count how many correct and incorrect answers was made.
+2. I can but a timer so that the player have a shorter time to search up the correct information.
 
 ### Media
 ![Photo](https://www.pexels.com/sv-se/)
--- assets/js/style.js
+++ assets/js/style.js
@@ -1,174 +1,213 @@
-// jshint esversion: 6
-const questionContainer = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-button");
-const nextButton = document.getElementById("next-btn");
-const resultDiv = document.getElementById("result");
-const restartButton = document.getElementById("restart-btn");
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-let shuffleQuestion, currentQuestion, score;
-// questions
-const question = [
-    {
-        question: "This character said this iconic line I, be back who said that?",
-        answers: [
-            { text: "Arnold Schwarzenegger", correct: true },
-            { text: "Sylvester Stallone", correct: false },
-            { text: "Bruce Willis", correct: false },
-            { text: "Jason Statham", correct: false },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
-        ],
-    },
-    {
-        question: "",
-        answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true },
-        ],
-    },
-];
+let shuffledQuestions, currentQuestionIndex
 
-startQuiz();
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-function startQuiz() {
-    score = 0;
-    questionContainer.style.display = "flex";
-    shuffleQuestion = question.sort(() => Math.random() - 0.5);
-    currentQuestion = 0;
-    nextButton.classList.remove("hide");
-    restartButton.classList.add("hide");
-    resultDiv.classList.add("hide");
-    setNextQuestion();
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-    resetState();
-    showQuestion(shuffleQuestion[currentQuestion]);
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach((answer, index)=> {
-        const inputGroup = document.createElement("div");
-        inputGroup.classList.add("input-group");
-
-        const radio = document.createElement("input");
-        radio.type = "radio";
-        radio.id = "answer" + index;
-        radio.name = "answer";
-        radio.value = index;
-
-        const label = document.createElement("label");
-        label.htmlFor = "answer" + index;
-        label.innerText = answer.text;
-    });
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
 function resetState() {
-    while (answerButton.firstChild) {
-        answerButton.removeChild(answerButton.firstChild);
-    }
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
 
-nextButton.addEventListener("click", () => {
-    const answerIndex = Array.from(
-        answerButton.querySelectorAll("input")
-    ).findIndex((radio) => radio.checked);
-    if (answerIndex !== -1) {
-        if (shuffleQuestion[currentQuestion].answer[answerIndex].
-            correct) {
-                score++;
-            }
-            currentQuestion++;
-            if (shuffleQuestion.length > currentQuestion) {
-                setNextQuestion();
-            } else {
-                endQuiz();
-            }
-    } else {
-        alert("Please select an answer.");
-    }
-});
-
-restartButton.addEventListener("click", startQuiz);
-
-function endQuiz() {
-    questionContainer.style.display = "none";
-    nextButton.classList.add("hide");
-    restartButton.classList.remove("hide");
-    resultDiv.classList.remove("hide");
-    resultDiv.innerText = `Your final score: ${score} / ${shuffleQuestion.length}`;
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
 }
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: "Who said this iconic line I be back?",
+    answers: [
+      { text: 'Arnold Schwarzenegger', correct: true },
+      { text: "Bruce Willis", correct: false },
+      { text: "Sylvester Stallone", correct: false },
+      { text: "Chuck Norris", correct: false }
+    ]
+  },
+  {
+    question: "What year did the First Back to the future release?",
+    answers: [
+      { text: 'July 1, 1985', correct: false },
+      { text: 'July 3, 1985', correct: false },
+      { text: 'July 9, 1985', correct: true },
+      { text: 'July 15, 1985', correct: false }
+    ]
+  },
+  {
+    question: "What was Disney's first long animated movie?",
+    answers: [
+      { text: "Dumbo", correct: false },
+      { text: "Snow White", correct: true },
+      { text: "Pinocchio", correct: false },
+      { text: "Bambi", correct: false }
+    ]
+  },
+  {
+    question: "What is the name of the Godfather?",
+    answers: [
+      { text: "Vito Corleone", correct: true },
+      { text: "Marlon Brando", correct: false }
+    ]
+  },
+  {
+    question: "What was Pixar first ever movie?",
+    answers: [
+      { text: "Bug's Life", correct: false },
+      { text: "Toy Story", correct: true },
+      { text: "Monsters, Inc.", correct: false },
+      { text: "Finding Nemo", correct: false }
+    ]
+  },
+  {
+    question: "What year did the first Pixar movie release?",
+    answers: [
+      { text: "November 2, 1993", correct: false },
+      { text: "November 24, 1996", correct: false },
+      { text: "November 25, 1998", correct: false },
+      { text: "November 22, 1995", correct: true }
+    ]
+  },
+  {
+    question: "What country was James Bond from?",
+    answers: [
+      { text: "Scotland", correct: true },
+      { text: "England", correct: false },
+      { text: "Ireland", correct: false },
+      { text: "Wales", correct: false }
+    ]
+  },
+  {
+    question: "Which movie did a hybrid of real and animated character?",
+    answers: [
+      { text: "Cool World", correct: true },
+      { text: "Marry Poppins", correct: true },
+      { text: "Who Framed Roger Rabbit", correct: true },
+      { text: "Space Jam", correct: true }
+    ]
+  },
+  {
+    question: "Which was the first animated movie to win a Oscar?",
+    answers: [
+      { text: "The Incredibles", correct: false },
+      { text: "Finding Nemo", correct: false },
+      { text: "Spirited Away", correct: false },
+      { text: "Shrek", correct: true }
+    ]
+  },
+  {
+    question: "Which director did the movie Jaws?",
+    answers: [
+      { text: "George Lucas", correct: false },
+      { text: "Steven Spielberg", correct: true },
+      { text: "Harrison Ford", correct: false },
+      { text: "Carrie Fisher", correct: false }
+    ]
+  },
+  {
+    question: "what year did star wars release?",
+    answers: [
+      { text: "16 december 1977", correct: true },
+      { text: "1 december 1977", correct: false },
+      { text: "26 december 1977", correct: false },
+      { text: "10 december 1977", correct: false }
+    ]
+  },
+  {
+    question: "Which movie comes this line from Follow the yellow brick road?",
+    answers: [
+      { text: "Dumbo", correct: false },
+      { text: "Wizard of OZ", correct: true },
+      { text: "Labyrinth", correct: false },
+      { text: "Goonies", correct: false }
+    ]
+  },
+  {
+    question: "What is the name of boss in the first Ghostbusters?",
+    answers: [
+      { text: "Gate Keeper", correct: false },
+      { text: "Gozer", correct: true },
+      { text: "Key Bearer", correct: false },
+      { text: "Bambi", correct: false }
+    ]
+  },
+  {
+    question: "What was the first Indiana Jones?",
+    answers: [
+      { text: "Kingdom of the Crystal Skull", correct: false },
+      { text: "Raiders of the Lost Ark", correct: true },
+      { text: "Temple of Doom", correct: false },
+      { text: "Last Crusade", correct: false }
+    ]
+  },
+  {
+    question: "In smokey and the bandit what was reg number on his car?",
+    answers: [
+      { text: "Ban1", correct: true },
+      { text: "Bandit", correct: false },
+      { text: "Smokey", correct: false },
+      { text: "Frog", correct: false }
+    ]
+  },
+]
