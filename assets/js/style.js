529798fdc538628a675d90050386e5c5fbab4ee4
Jan-Robert Nielsen <robertnielsen33@gmail.com>
2024-05-11 13:42:25 +0000

    added the function to display the quiz and calculte the score

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,9 +1,9 @@
-// Define the Quiz questions and answers
+// Define the Quiz questions d answers
 
 const Questions = [
   {
-    Question 1: What is the Capital of Sweden?,
-    Answer: {
+    Question: "What is the Capital of Sweden?",
+    answers: {
       a: "Dublin",
       b: "Stockholm",
       c: "Tokyo",
@@ -12,8 +12,8 @@ const Questions = [
     correctAnswer: "b"
   },
   {
-    Question 2: What was Disney movie the saved them?,
-    Answer: {
+    Question: "What was Disney movie the saved them?",
+    answers: {
       a: "SnowWhite",
       b: "Steamboat willie",
       c: "Pinocchio",
@@ -22,8 +22,8 @@ const Questions = [
     correctAnswer: "a"
   },
   {
-    Question 3: who sing the song Tutti Frutti 1955?
-    Answer: {
+    Question: "who sing the song Tutti Frutti 1955?",
+    answers: {
       a: "Little Richard",
       b: "Elvis Prasley",
       c: "Paul Anka",
@@ -33,5 +33,39 @@ const Questions = [
   }
 ];
 
+// Function to display the quiz
+function displayQuiz() {
+  const quizContainer = document.getElementById('quiz');
+  let quizHtml = '';
 
+  questions.forEach((question, index) => {
+      quizHtml += `<div>
+                      <p>${index + 1}. ${question.question}</p>
+                      <ul>
+                          <li><input type="radio" name="question${index}" value="a">${question.answers.a}</li>
+                          <li><input type="radio" name="question${index}" value="b">${question.answers.b}</li>
+                          <li><input type="radio" name="question${index}" value="c">${question.answers.c}</li>
+                          <li><input type="radio" name="question${index}" value="c">${question.answers.d}</li>
+                      </ul>
+                  </div>`;
+  });
+
+  quizContainer.innerHTML = quizHtml;
+}
+
+// Function to check answers and calculate score
+
+function checkAnswer() {
+  let score = 0;
+
+  question.forEach((question, index) => {
+    const selectedAnswer = document.querySelector(`input[name="question${index}"]:checked`);
+    if (selectedAnswer) {
+      if (selectedAnswer.value === question.correctAnswer) {
+        score++;
+      }
+    }
+  });
+  alert(`Your score is: ${score}/${questions.length}`);
+}
 
